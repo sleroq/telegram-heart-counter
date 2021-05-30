e3c73cca930b9a6396041d4818507b0d88febec7
sleeper_ok <52239427+iamdowner@users.noreply.github.com>
2021-05-30 16:38:08 +0300

    added if statement to prevent unlikely errors & code prettify

--- src/buttons/index.ts
+++ src/buttons/index.ts
@@ -7,9 +7,10 @@ import { show_details } from './show_details'
 export async function cb_query_handler(ctx: any) {
     const query_data = ctx.update.callback_query.data;
     const chat_id = ctx.chat.id
-    // if (ctx.update.callback_query && ctx.update.callback_query.date) {
-    //     return
-    //   }
+
+    if (!db[chat_id]) {
+        return
+    }
     ctx.answerCbQuery().catch((err: any) => {
         console.log(err);
     });
@@ -26,18 +27,20 @@ export async function cb_query_handler(ctx: any) {
     }
     if (query_data.match(/^askdel_\d+/)) {
         const index = query_data.split('_')[1]
-        const are_u_sure = "Are you sure you want to delete " + db[chat_id].counters[index].heart + " counter?"
+        if (!db[chat_id].counters[index]) { return }
+        const are_u_sure = "Are you sure you want to *delete* " + db[chat_id].counters[index].heart + " counter?"
         ctx.editMessageText(
             escape_markdown(are_u_sure), {
             parse_mode: "MarkdownV2",
             reply_markup: {
-                inline_keyboard: [{ text: "Yes", callback_data: "del_" + index }, { text: "No", callback_data: "show_" + index }]
+                inline_keyboard: [[{ text: "Yes", callback_data: "del_" + index }, { text: "No", callback_data: "show_" + index }]]
             }
-        }).catch((err:any)=>console.log(err))
+        }).catch((err: any) => console.log(err))
         return
     }
     if (query_data.match(/^del_\d+/)) {
         const index = query_data.split('_')[1]
+        if (!db[chat_id].counters[index]) { return }
         db[chat_id].counters.splice(index, 1)
         await update_db(db)
         await edit_count_message(ctx, ctx.update.callback_query.message.message_id, db)
@@ -54,8 +57,8 @@ export async function cb_query_handler(ctx: any) {
             escape_markdown(creating_counter_message), {
             parse_mode: "MarkdownV2",
             reply_markup: {
-                inline_keyboard: [[{ text: " <- back", callback_data: "del_" + newest_counter_id}]]
+                inline_keyboard: [[{ text: " <- back", callback_data: "del_" + newest_counter_id }]]
             }
-        }).catch((err:any)=>console.log(err))
+        }).catch((err: any) => console.log(err))
     }
 }
